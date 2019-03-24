3ad21015a5c7641fed00dbf45d9351abd6d71aa2
AayushDongre <44211899+AayushDongre@users.noreply.github.com>
2019-03-24 16:04:24 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,159 @@
+import cv2
+import numpy as np
+import os
+import pickle
+from random import shuffle
+
+train_dir = "C:/Users/student/Desktop/Hackathon/train_data/train"
+test_dir = "C:/Users/student/Desktop/Hackathon/test_data/test"
+image_size = 256
+LR = 1e-3
+
+def label(index):
+    if index == '0':
+        return [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '1':
+        return [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '2':
+        return [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '3':
+        return [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '4':
+        return [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '5':
+        return [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '6':
+        return [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '7':
+        return [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '8':
+        return [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '9':
+        return [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '10':
+        return [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '11':
+        return [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '12':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '13':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '14':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '15':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '16':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '17':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '18':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '19':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '20':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '21':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '22':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '23':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '24':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '25':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '26':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
+    if index == '27':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
+    if index == '28':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
+    if index == '29':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]
+    if index == '30':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
+    if index == '31':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
+    if index == '32':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0]
+    if index == '33':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0]
+    if index == '34':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
+    if index == '35':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]
+    if index == '36':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
+    if index == '37':
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+def create_data_set():
+    training_data = []
+    for i in range(38):
+        print("current i="+str(i))
+        cur_dir = train_dir + "/c_{}".format(i)
+        for img in os.listdir(cur_dir):
+            label = str(i)
+            path = os.path.join(cur_dir,img)
+            img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(image_size,image_size))
+            training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    print(training_data[0])
+    with open('training_dataB.pkl', 'wb') as f:
+        pickle.dump(training_data, f)
+    return training_data
+print("loading data")
+with open('training_dataB.pkl', 'rb') as f:
+    training_data = pickle.load(f)
+
+print("data loaded")
+
+train = training_data[:-500]
+test = training_data[-500:]
+
+train_x = np.array([i[0] for i in train]).reshape(-1,image_size,image_size,1)
+y_train = [label(i[1]) for i in train]
+train_y = np.array([i for i in y_train])
+
+test_x = np.array([i[0] for i in test]).reshape(-1,image_size,image_size,1)
+y_test = [label(i[1]) for i in test]
+test_y = np.array([i for i in y_test])
+
+print(np.shape(test_x))
+print(np.shape(test_y))
+print(np.shape(train_x))
+print(train_y[0])
+
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+convnet = input_data(shape=[None, 256, 256, 1], name='input')
+
+print(np.shape(convnet))
+
+convnet = conv_2d(convnet, 64,20,strides=1, activation='relu')
+print(np.shape(convnet))
+convnet = max_pool_2d(convnet, 2,2)
+print(np.shape(convnet))
+
+convnet = conv_2d(convnet, 64, 20,strides=1, activation='relu')
+print(np.shape(convnet))
+convnet = max_pool_2d(convnet, 2,2)
+print(np.shape(convnet))
+
+convnet = conv_2d(convnet, 128, 20,strides=1, activation='relu')
+print(np.shape(convnet))
+convnet = max_pool_2d(convnet, 2,2)
+print(np.shape(convnet))
+
+convnet = fully_connected(convnet, 2048, activation='relu')
+convnet = dropout(convnet, 0.8)
+print(np.shape(convnet))
+convnet = fully_connected(convnet, 38, activation='softmax')
+
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+print(np.shape(convnet))
+model = tflearn.DNN(convnet)
+print(model)
+model.fit({'input': train_x}, {'targets': train_y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}),
+    snapshot_step=500, show_metric=True, run_id='mnist')
